161aaa93203fbee05295615555099b8ee737d6ce
Laminek123 <66846564+Laminek123@users.noreply.github.com>
2022-12-20 09:56:05 +0600

    Add files via upload

---
+++ Сессия 2/SQLQuery2.sql
@@ -0,0 +1,6 @@
+create table Абонент
+(
+Id_Абонента int primary key,
+ФИО varchar(50),
+Адрес_проживания varchar(50),
+)
---
+++ Сессия 2/SQLQuery5.sql
@@ -0,0 +1,7 @@
+create table Телефон
+(
+Номер_Телефона varchar(12) primary key,
+Номер_договора varchar(50),
+Дата_установки varchar(50),
+Id_абонента int
+)
