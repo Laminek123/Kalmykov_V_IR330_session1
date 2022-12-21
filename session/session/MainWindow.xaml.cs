6f7f7c01351bc985a2547432a7aff42c73565570
Laminek123 <66846564+Laminek123@users.noreply.github.com>
2022-12-21 13:57:27 +0600

    Add files via upload

---
+++ session/session/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace session
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Frame1.Navigate(new журнал_разговоров());
+            Class1.Frame1 = new Frame();
+        }
+
+        private void button1_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
